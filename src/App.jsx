e8d23dc1cdb1d94e88451eb33b8c0191add188af
rubyaj24 <amaljithmvinod@gmail.com>
2025-03-14 12:50:53 +0530

    Add TableDemo component and integrate it into App; update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import React from 'react'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { TableDemo } from './components/TableDemo'
 
 
 
@@ -15,18 +8,7 @@ const App = () => {
     <div>
       <h1 className='text-2xl bg-red-500'>Sargam 2025</h1>
       <div className='flex justify-center items-center min-h-screen mx-auto'>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
+        <TableDemo />
       </div>
     </div>
   )
